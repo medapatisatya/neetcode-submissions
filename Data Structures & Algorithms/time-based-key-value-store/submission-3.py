79ff53feb66b2106c3edb54e5666fd2f2855ce1e
Medapati Satya Vijaya Saradhi Reddy <msvsrmedapati@gmail.com>
2026-07-26 12:26:16 +0530

    Add: time-based-key-value-store - submission-3

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-3.py
@@ -0,0 +1,19 @@
+import bisect
+class TimeMap:
+
+    def __init__(self):
+        self.timemap = {}
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key in self.timemap:
+            self.timemap[key].append([value, timestamp])
+        else:
+            self.timemap[key] = [[value, timestamp]]
+
+    def get(self, key: str, timestamp: int) -> str:
+        if key not in self.timemap: return ""
+        index = bisect.bisect_right(self.timemap[key], timestamp, key=lambda x: x[1]) - 1
+
+        if index >= 0:
+            return self.timemap[key][index][0]
+        return ""
